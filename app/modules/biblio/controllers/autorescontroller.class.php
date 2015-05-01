e2978a9b2225da58bdb233e2ea835f134cb5168b
elkinlrc <elkinlrc@gmail.com>
2015-05-01 17:26:56 -0500

    se hizo que los controladores fueran genericos el primer cambio se hicieron como prueba piloto en autores

--- app/modules/biblio/controllers/autorescontroller.class.php
+++ app/modules/biblio/controllers/autorescontroller.class.php
@@ -8,19 +8,18 @@ class Modules_Biblio_Controllers_AutoresController {
     private $_parameters;
     private $_pathConfig;
     private $_action;
+    public $_module="autores";
 
     public function __construct($param, $dom, $path) {
       
         $this->_parameters = $param;
         $this->_action = $param->get_parameter("action", "");
         $action = $this->_action;
-        $rc = new ReflectionClass("Modules_Biblio_Controllers_AutoresController");
+        $rc = new ReflectionClass("Modules_Biblio_Controllers_".$this->_module."Controller");
         if ($rc->hasMethod($action)) {
          
             $this->_dom = $dom;
             $this->_pathConfig = $path;
-           
-            //var_dump($path);
             $this->$action();
           
                
@@ -49,7 +48,7 @@ class Modules_Biblio_Controllers_AutoresController {
 
         $cadenaurl = $this->_parameters->KeyGen();
         $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/biblio/views/autores/crear.php?";
+        $vista = "/biblio/views/".$this->_module."/crear.php?";
 //   exit();
         header("Location: {$ruta}{$vista}{$cadenaurl}"); //Arma la ruta
     }
@@ -62,199 +61,51 @@ class Modules_Biblio_Controllers_AutoresController {
         $this->_parameters->add("Sw", $palabraBusqueda);
         $cadenaurl = $this->_parameters->KeyGen();      //mensaje si se modifica la url
         $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/biblio/views/autores/index.php?";
+        $vista = "/biblio/views/".$this->_module."/index.php?";
         header("Location: {$ruta}{$vista}{$cadenaurl} ");
     }
 
     private function eliminar() {
-        $obj = new Modules_Empresa_Model_Sedes();
-        $obj = $this->_parameters->set_object($obj);
+        $obj = new Modules_Biblio_Model_Autores();
+        $objo = $this->_parameters->set_object($objo);
         $facade = new Modules_Empresa_Model_Sedesfacade();
         $mensaje = 32;
-        if ($facade->delete($obj)) {
-
-            $objaudi = new Modules_Empresa_Model_Auditoria();
-            $objaudi->set_nombretabla("Sedes");
-            $objaudi->set_tipooperacion(2);
-            $objaudi->set_codregistro($obj->get_codsede());
-
-            $facadeaudi = new Modules_Empresa_Model_AuditoriaFacade();
-
-            $facadeaudi->add($objaudi);
-
-
-//Si el add fue correcto coloca el mensaje
+        if ($facade->delete($objo)) {
             $mensaje = 12;
         }
         $this->_parameters->delete_all();
         $this->_parameters->add("msg", $mensaje);
-        $this->_parameters->add("codsede", $obj->get_codsede());
+        $this->_parameters->add("codsede", $obj->get_codautor());
 
         $cadenaurl = $this->_parameters->KeyGen();
         $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/empresa/views/sedesAdmin.php?";
+        $vista = "/biblio/views/".$this->_module."/index.php?";
 //      exit();
         header("Location: {$ruta}{$vista}{$cadenaurl}"); //Arma la ruta
     }
 
     private function editar() {
-        $obj = new Modules_Empresa_Model_Sedes();
-        $obj = $this->_parameters->set_object($obj);
-        $facade = new Modules_Empresa_Model_Sedesfacade();
+        $obj = new Modules_Biblio_Model_Autores();
+        $objo = $this->_parameters->set_object($obj);
+        $facade = new Modules_Biblio_Model_AutoresFacade();
         $mensaje = 32;
-        if ($facade->update($obj)) {
+        if ($facade->update($objo)) {
 
-            $objaudi = new Modules_Empresa_Model_Auditoria();
-            $objaudi->set_nombretabla("Sedes");
-            $objaudi->set_tipooperacion(3);
-            $objaudi->set_codregistro($obj->get_codsede());
-
-            $facadeaudi = new Modules_Empresa_Model_AuditoriaFacade();
-
-            $facadeaudi->add($objaudi);
-
-
-//Si el add fue correcto coloca el mensaje
+        
             $mensaje = 12;
         }
         $this->_parameters->delete_all();
         $this->_parameters->add("msg", $mensaje);
-        $this->_parameters->add("codsede", $obj->get_codsede());
+        $this->_parameters->add("codsede", $obj->get_codautor());
 
         $cadenaurl = $this->_parameters->KeyGen();
         $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/empresa/views/sedesAdmin.php?";
+        $vista = "/biblio/views/editar.php?";
 //        exit();
         header("Location: {$ruta}{$vista}{$cadenaurl}"); //Arma la ruta
     }
 
-    private function sedesBuscar() {
-
-        $opcionBusqueda = $this->_parameters->get_parameter("So", "");
-        $palabraBusqueda = $this->_parameters->get_parameter("Sw", "");
-        $this->_parameters->delete_all(); //cuando termine de ahcer el proceso limpied get parametrer
-        $this->_parameters->add("So", $opcionBusqueda);
-        $this->_parameters->add("Sw", $palabraBusqueda);
-        $cadenaurl = $this->_parameters->KeyGen();      //mensaje si se modifica la url
-        echo "hola mundo";
-        //  exit();
-        $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/empresa/views/sedesListar.php?";
-        header("Location: {$ruta}{$vista}{$cadenaurl} ");
-    }
-
-    /* ------------------------------------------------------------------------- */
-    /* ------------------------------------------------------------------------- */
-    /* ------------------------------------------------------------------------- */
-    /* ----------------------------SÚPER ADMINISTRADOR-------------------------- */
-    /* ------------------------------------------------------------------------- */
-    /* ------------------------------------------------------------------------- */
-
-    private function crearSedesSuper() {
-        $obj = new Modules_Empresa_Model_Sedes();
-        $obj = $this->_parameters->set_object($obj);
-        $facade = new Modules_Empresa_Model_Sedesfacade();
-        $mensaje = 31;
-        if ($facade->add($obj)) {
-            $objaudi = new Modules_Empresa_Model_Auditoria();
-            $objaudi->set_nombretabla("Sedes");
-            $objaudi->set_tipooperacion(1);
-            $objaudi->set_codregistro($obj->get_codsede());
-
-            $facadeaudi = new Modules_Empresa_Model_AuditoriaFacade();
-
-            $facadeaudi->add($objaudi);
-            //Si el add fue correcto coloca el mensaje
-            $mensaje = 11;
-        }
-        $this->_parameters->delete_all();
-        $this->_parameters->add("msg", $mensaje);
-        $this->_parameters->add("codsede", $obj->get_codsede());
-
-        $cadenaurl = $this->_parameters->KeyGen();
-        $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/empresa/views/sedesAdminSuper.php?";
-        //   exit();
-//        header("Location: {$ruta}{$vista}{$cadenaurl}"); //Arma la ruta
-        $script = "<script>";
-        $script.="window.parent.location.href=\"{$ruta}{$vista}{$cadenaurl}\"";
-        $script.="</script>";
-        echo $script;
-    }
-
-    private function eliminarSedesSuper() {
-        $obj = new Modules_Empresa_Model_Sedes();
-        $obj = $this->_parameters->set_object($obj);
-        $facade = new Modules_Empresa_Model_Sedesfacade();
-        $mensaje = 32;
-        if ($facade->delete($obj)) {
-
-            $objaudi = new Modules_Empresa_Model_Auditoria();
-            $objaudi->set_nombretabla("Sedes");
-            $objaudi->set_tipooperacion(2);
-            $objaudi->set_codregistro($obj->get_codsede());
-
-            $facadeaudi = new Modules_Empresa_Model_AuditoriaFacade();
-
-            $facadeaudi->add($objaudi);
-
-
-//Si el add fue correcto coloca el mensaje
-            $mensaje = 12;
-        }
-        $this->_parameters->delete_all();
-        $this->_parameters->add("msg", $mensaje);
-        $this->_parameters->add("codsede", $obj->get_codsede());
-
-        $cadenaurl = $this->_parameters->KeyGen();
-        $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/empresa/views/sedesAdminSuper.php?";
-//      exit();
-//        header("Location: {$ruta}{$vista}{$cadenaurl}"); //Arma la ruta
-        $script = "<script>";
-        $script.="window.parent.location.href=\"{$ruta}{$vista}{$cadenaurl}\"";
-        $script.="</script>";
-        echo $script;
-    }
-
-    private function actualizarSedesSuper() {
-        $obj = new Modules_Empresa_Model_Sedes();
-        $obj = $this->_parameters->set_object($obj);
-        $facade = new Modules_Empresa_Model_Sedesfacade();
-        $mensaje = 32;
-        if ($facade->update($obj)) {
-
-            $objaudi = new Modules_Empresa_Model_Auditoria();
-            $objaudi->set_nombretabla("Sedes");
-            $objaudi->set_tipooperacion(3);
-            $objaudi->set_codregistro($obj->get_codsede());
-
-            $facadeaudi = new Modules_Empresa_Model_AuditoriaFacade();
-
-            $facadeaudi->add($objaudi);
-
-
-//Si el add fue correcto coloca el mensaje
-            $mensaje = 12;
-        }
-        $this->_parameters->delete_all();
-        $this->_parameters->add("msg", $mensaje);
-        $this->_parameters->add("codsede", $obj->get_codsede());
-
-        $cadenaurl = $this->_parameters->KeyGen();
-        $ruta = $this->_pathConfig["ROOT"]["modules"];
-        $vista = "/empresa/views/sedesAdminSuper.php?";
-//        exit();
-//        header("Location: {$ruta}{$vista}{$cadenaurl}"); //Arma la ruta
-        $script = "<script>";
-        $script.="window.parent.location.href=\"{$ruta}{$vista}{$cadenaurl}\"";
-        $script.="</script>";
-
-        echo $script;
-        
-        
-    }
+   
 
 }
 
-?>
