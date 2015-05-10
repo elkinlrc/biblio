a69ad889e25b6de258d4d56fe70886c43503cb6a
elkinlrc <elkinlrc@gmail.com>
2015-05-09 21:38:37 -0500

    trabajando en libros

--- app/modules/biblio/views/xhtmls/view_index.php
+++ app/modules/biblio/views/xhtmls/view_index.php
@@ -14,10 +14,10 @@ if (!isset($id_security)) {
                         
                         <form id="frm" name="frm" method="POST" action="traceo.php" onsubmit="javascript:return checkform('frm');">
                             <input type="hidden" id="SECURITY_ID" name="SECURITY_ID" value="false"/>
-                            <input type="hidden" id="action" name="action" value="crearAutores" />
-                            <input type="hidden" id="controller" name="controller" value="Biblio/AutoresController" />
-                            <img src="<?=$PATH_CONFIG["ROOT"]["images"];?>/LogoHorizontal-UTS.png" />
-                            <input type="text" class="input-xxlarge" style="width: 530px;" id="buscado" name="buscador" value="" />
+                            <input type="hidden" id="action" name="action" value="buscar" />
+                            <input type="hidden" id="controller" name="controller" value="Biblio/LibrosController" />
+                            <img src="<?=$PATH_CONFIG["ROOT"]["images"];?>/LogoHorizontal-UTS.png"  />
+                            <input type="text" class="input-xxlarge" style="width: 530px;" id="buscado" name="buscador"  />
                             <input type="submit" class=" btn btn-success" value="Buscar">
                             <ul>
                                 <li>Busqueda Basica</li>
